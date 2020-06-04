645dd6d9485ecedcdda3b7bada8f6df7a4ec630d
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-06-04 19:27:40 +0530

    Question 4

---
+++ Question4.py
@@ -0,0 +1,9 @@
+#Q 4 reverse a array of characters
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
