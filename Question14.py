956f4504f3db358f9b120dfb3c1e7e32d46cc39a
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-06-15 11:09:48 +0530

    question 14

--- Question14.py
+++ Question14.py
@@ -1,26 +1,30 @@
 # Question 14 : Cheapest Flights Within K Stops
+#finds best route under parameter k using dfs with pruning 
 import sys
 class Solution:
     def __init__(self):
         self.flights = None
         self.cost = sys.maxsize
     def findroute(self, src, dst, k, curr_cost, visited):
-        print(src, dst, curr_cost)
+       # print(src, dst, curr_cost)
         while(True):
             if(k<0):
                 break
             
             elif(self.cost < curr_cost):
+                #stops searcing for new possible path from previous node if cost is greater than minimum cost evaluated
                 return True
                 break
             
             elif(src == dst):
+                #stop searching for new possible path from previous node if destination is arrived
                 if(curr_cost < self.cost):
                     self.cost = curr_cost
                     return True
                 break
             
             else:
+                #find all possible path from current node and sort them in ascending order
                 possible_paths = sorted([m for m in self.flights if(m[0]==src)], key = lambda x: x[2])
                 for i in possible_paths:
                     visitedtemp = visited.copy()
