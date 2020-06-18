c9889022febcd946404b31aa96d1c1508c5afab4
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-06-18 16:52:49 +0530

    question 18

---
+++ Question18.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        left = 0
+        length = len(citations)
+        right = length-1
+        if(len(citations)==0) : return 0
+        if(len(citations)==1 and citations[0]!=0) : return 1
+        if(len(citations)==1 and citations[0]==0): return 0
+        while(left<=right):
+            mid = int((left+right)/2)
+            print(left, right, mid)
+            if(citations[mid]<length-mid):
+                left = mid+1
+            else:
+                right = mid-1
+        return length-left
