d8f9488a8681df67afdd97f8a88c89505a8e4757
prakhar kopergaonkar <pkopergaonkar@gmail.com>
2020-06-25 13:19:42 +0530

    question 25

---
+++ Question25.py
@@ -0,0 +1,8 @@
+# Question 25: Find the Duplicate Number
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in range(0,len(nums)):
+            if(nums[abs(nums[i])]<0):
+                return abs(nums[i])
+            else:
+                nums[abs(nums[i])] = -nums[abs(nums[i])]
